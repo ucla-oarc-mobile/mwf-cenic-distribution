092759c323b0ea45567261b18b75554f0152196f
Ed Sakabu <EdSakabu@ucla.edu>
2013-06-12 16:44:23 -0700

    DEBUG flag added for diagnostics

--- test.bash
+++ test.bash
@@ -1,35 +1,39 @@
 #!/bin/bash
 
+unset DEBUG
+
 # configureation and replace function are in the config.txt file that is sourced below
 . config.txt
 
 echo "Starting.."
+if [ $DEBUG ] ; then echo "Debuging on..." ; fi
 
 
 for host in "${!HOSTS[@]}"
 do
-  echo $host
+  if [ $DEBUG ] ; then echo -n "$host is ";  fi
   if [ ${HOSTS[$host]} = "active" ]
   then
-    echo $host is ACTIVE
+    if [ $DEBUG ] ; then echo "ACTIVE" ; fi
     assoc_array_string=$(declare -p $host)
     eval "declare -A new_host_array="${assoc_array_string#*=}
 # set up the variables for substitution and creation of the config files like
 #  path=/var/www/html/mwf/ucla.prod
     for X in "${!new_host_array[@]}"
       do
-        echo host variable = $new_host_array[$X]
+        if [ $DEBUG ] ; then echo host variable = $new_host_array[$X] ; fi
         eval $X=${new_host_array[$X]}
       done
     
      for file_base in ${!config_files[@]}
        do
+        if [ $DEBUG ] ; then echo "Original config string " ${config_files[$file_base]} ; fi
         file_in=$(replace ${config_files[$file_base]})
-        echo ./templates/$file_base goes to  $(replace ${config_files[$file_base]})
-        echo "base = $file_base , file = $file_in"
+        if [ $DEBUG ] ; then echo ./templates/$file_base goes to  $(replace ${config_files[$file_base]}) ; fi
+        if [ $DEBUG ] ; then echo "base = $file_base , file = $file_in" ; fi
        done
   else
-   echo "Host $host is NOT active"
+   if [ $DEBUG ] ; then echo "NOT active" ; fi
   fi
 done  
 exit
