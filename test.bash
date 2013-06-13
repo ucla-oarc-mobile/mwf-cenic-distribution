643546db0961ffe3a9fec862cb6259c09b52b8c7
Ed Sakabu <EdSakabu@ucla.edu>
2013-06-12 17:58:15 -0700

    Error handling

--- test.bash
+++ test.bash
@@ -1,41 +1,56 @@
 #!/bin/bash
-
-unset DEBUG
-
-# configureation and replace function are in the config.txt file that is sourced below
+# configuration and replace function are in the config.txt file that is sourced below
+# in addition to the lib.trap.sh library that needs to be sources first thing
 . config.txt
 
+
+DEBUG=false
+
 echo "Starting.."
-if [ $DEBUG ] ; then echo "Debuging on..." ; fi
+if [ $DEBUG = "true" ] ; then echo "Debuging on..." ; fi
 
-
+# walk thru the hosts in $HOSTS and process the ones makred "active"
 for host in "${!HOSTS[@]}"
 do
-  if [ $DEBUG ] ; then echo -n "$host is ";  fi
+  if [ $DEBUG = "true" ] ; then echo -n "$host is ";  fi
   if [ ${HOSTS[$host]} = "active" ]
   then
-    if [ $DEBUG ] ; then echo "ACTIVE" ; fi
+    if [ $DEBUG = "true" ] ; then echo "ACTIVE" ; fi
+# 
     assoc_array_string=$(declare -p $host)
+    if [ $DEBUG = "true" ] ; then echo "associative array for the host $host is defined with \"$assoc_array_string\""; fi
     eval "declare -A new_host_array="${assoc_array_string#*=}
 # set up the variables for substitution and creation of the config files like
 #  path=/var/www/html/mwf/ucla.prod
     for X in "${!new_host_array[@]}"
       do
-        if [ $DEBUG ] ; then echo host variable = $new_host_array[$X] ; fi
+        if [ $DEBUG = "true" ] ; then echo host variable = $new_host_array[$X] ; fi
         eval $X=${new_host_array[$X]}
       done
     
      for file_base in ${!config_files[@]}
        do
-        if [ $DEBUG ] ; then echo "Original config string " ${config_files[$file_base]} ; fi
+        if [ $DEBUG = "true" ] ; then echo "Original config string " ${config_files[$file_base]} ; fi
         file_in=$(replace ${config_files[$file_base]})
-        if [ $DEBUG ] ; then echo ./templates/$file_base goes to  $(replace ${config_files[$file_base]}) ; fi
-        if [ $DEBUG ] ; then echo "base = $file_base , file = $file_in" ; fi
+        if [ $DEBUG = "true" ] ; then echo ./templates/$file_base goes to  $(replace ${config_files[$file_base]}) ; fi
+        if [ $DEBUG = "true" ] ; then echo "base = $file_base , file = $file_in" ; fi
+# process template files ($TEMPLATEDIR/$file_base) and put them into the temp directory with the names $file_base.tmp
+# remove the temp file before writing into it
+     rm $TMPDIR/${file_base}.tmp
+     while read line
+       do
+         replace $line
+       done < $TEMPLATEDIR/$file_base >> $TMPDIR/${file_base}.tmp
+# move the file into place
+#     mv $TMPDIR/${file_base}.tmp $file_in
+# remove .tmp files
+#     rm $TMPDIR/${file_base}.tmp
        done
   else
-   if [ $DEBUG ] ; then echo "NOT active" ; fi
+   if [ $DEBUG = "true" ] ; then echo "NOT active" ; fi
   fi
 done  
+
 exit
 
 
