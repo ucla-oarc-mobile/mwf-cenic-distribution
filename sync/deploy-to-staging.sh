8d31bf445e37df035724c0ffce4712f94110ef8d
Ed Sakabu <edsakabu@ucla.edu>
2013-08-23 19:12:33 +0000

    added 1.2 distribution (hard coded)

--- sync/deploy-to-staging.sh
+++ sync/deploy-to-staging.sh
@@ -15,3 +15,16 @@ rsync -e "ssh -i /root/cenic-mwf.pem" --del -a -r $MWF_APACHE_CONFIGS1 root@$i:$
 rsync -e "ssh -i /root/cenic-mwf.pem" --del -a -r $MWF_APACHE_CONFIGS2 root@$i:$MWF_APACHE_CONFIGS2
 ssh -i /root/cenic-mwf.pem root@$i "service httpd reload"
 done
+
+MWF_BASE=/var/www/mwf/ucla_12.stage
+MWF_APACHE_CONFIGS1=/etc/httpd/sites-enabled/
+MWF_APACHE_CONFIGS2=/etc/httpd/sites-available/
+tar czvf /deploy/Staging-mwf-current.tgz $MWF_BASE
+cp /deploy/Staging-mwf-current.tgz /var/www/html/
+
+for i in `/usr/local/bin/instance-info2.sh -t Staging -s mwf`;do
+rsync -e "ssh -i /root/cenic-mwf.pem" --del -a -r $MWF_BASE/* root@$i:$MWF_BASE/
+rsync -e "ssh -i /root/cenic-mwf.pem" --del -a -r $MWF_APACHE_CONFIGS1 root@$i:$MWF_APACHE_CONFIGS1
+rsync -e "ssh -i /root/cenic-mwf.pem" --del -a -r $MWF_APACHE_CONFIGS2 root@$i:$MWF_APACHE_CONFIGS2
+ssh -i /root/cenic-mwf.pem root@$i "service httpd reload"
+done
